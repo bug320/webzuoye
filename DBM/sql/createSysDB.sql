8c394f35b0fef6e2753812487c729c2f91ff5f38
bug320 <1046218884@qq.com>
2017-06-06 21:56:26 +0800

    Fisrt MySQL

--- DBM/sql/createSysDB.sql
+++ DBM/sql/createSysDB.sql
@@ -1,7 +1,10 @@
 CREATE TABLE SYS(
     loopSize CHAR(2),
-    lengthSize CHAR(2)
+    lengthSize CHAR(2),
+    PRIMARY KEY(loopSize,lengthSize)
 );
+
+#CONSTRAINT pk_PersonID PRIMARY KEY (P_Id,LastName)
 
 INSERT INTO SYS(loopSize,lengthSize) VALUES ("7","4");
 
